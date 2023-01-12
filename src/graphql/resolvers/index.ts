de9ebc64f00c4ab9f1b3c76666d639bdf94809d9
Xenon-00 <faqihfirmansyah350@gmail.com>
2023-01-12 10:27:53 +0700

    add message resolver

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,13 +1,15 @@
 import userResolvers from "./user";
 import conversationResolvers from "./conversations";
 import scalarResolvers from "./scalars";
+import messageResolvers from "./message";
 import merge from "lodash.merge";
 
 const resolvers = merge(
   {},
   userResolvers,
   conversationResolvers,
-  scalarResolvers
+  scalarResolvers,
+  messageResolvers
 );
 
 export default resolvers;
