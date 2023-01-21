108074987ab7e68693862f2e0c58a51730eb3321
Xenon-00 <faqihfirmansyah350@gmail.com>
2023-01-21 16:22:38 +0700

    add new typeDefs

--- src/graphql/typeDefs/conversations.ts
+++ src/graphql/typeDefs/conversations.ts
@@ -2,21 +2,20 @@ import gql from "graphql-tag";
 
 const typeDefs = gql`
   scalar Date
-
-  type Mutation {
-    createConversation(participantIds: [String]): CreateConversationResponse
+  type Conversation {
+    id: String
+    latestMessage: Message
+    participants: [Participant]
+    createdAt: Date
+    updatedAt: Date
   }
 
   type CreateConversationResponse {
     conversationId: String
   }
 
-  type Conversation {
-    id: String
-    latestMessage: Message
-    participants: [Participant]
-    createdAt: Date
-    updateAt: Date
+  type ConversationUpdatedSubscriptionPayload {
+    conversation: Conversation
   }
 
   type Participant {
@@ -25,12 +24,24 @@ const typeDefs = gql`
     hasSeenLatestMessage: Boolean
   }
 
+  type Mutation {
+    createConversation(participantIds: [String]): CreateConversationResponse
+  }
+
+  type Mutation {
+    markConversationAsRead(userId: String!, conversationId: String!): Boolean
+  }
+
   type Query {
     conversations: [Conversation]
   }
 
   type Subscription {
     conversationCreated: Conversation
+  }
+
+  type Subscription {
+    conversationUpdated: ConversationUpdatedSubscriptionPayload
   }
 `;
 
